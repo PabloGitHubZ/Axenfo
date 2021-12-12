961bc8524a4c69a7deb25afa64a8d6845dc1ac6b
PabloGitHubZ <61061897+PabloGitHubZ@users.noreply.github.com>
2021-12-12 10:14:51 +0100

    Update datosPrueba.sql

--- AxenfoLite/AxenfoLite/sql/datosPrueba.sql
+++ AxenfoLite/AxenfoLite/sql/datosPrueba.sql
@@ -44,15 +44,15 @@ INSERT INTO switches (id, nombre, ip, marca, modelo, numero_serie)
 
 INSERT INTO nodos (id, nombre, ubicacion, direccion_fisica, latitud, longitud, controladora, switch, olt, estado, pendiente)
   VALUES ('1', 'Nodo Santiago', 'Santiago de Compostela' , 'rua castelao 12' , '42.879378', '-8.551017', '1' , '1' , '1', 'Funcionando', 'Nada Pendiente');
-INSERT INTO nodos (nombre, ubicacion, direccion_fisica, latitud, longitud, controladora, switch, olt, estado, pendiente)
+INSERT INTO nodos (id, nombre, ubicacion, direccion_fisica, latitud, longitud, controladora, switch, olt, estado, pendiente)
   VALUES ('2', 'Nodo Vigo', 'Vigo' , 'rua emilia pardo bazan 3' , '42.232302', '-8.726606', '2' , '2' , '2', 'En construcción', 'Instalación Equipos');
-INSERT INTO nodos (nombre, ubicacion, direccion_fisica, latitud, longitud, controladora, switch, olt, estado, pendiente)
+INSERT INTO nodos (id, nombre, ubicacion, direccion_fisica, latitud, longitud, controladora, switch, olt, estado, pendiente)
   VALUES ('3', 'Nodo A Coruña', 'A Coruña' , 'rua do faro 44' , '43.376055', '-8.437614', '3' , '3' , '3', 'Funcionando', 'Nada Pendiente');
-INSERT INTO nodos (nombre, ubicacion, direccion_fisica, latitud, longitud, controladora, switch, olt, estado, pendiente)
+INSERT INTO nodos (id, nombre, ubicacion, direccion_fisica, latitud, longitud, controladora, switch, olt, estado, pendiente)
   VALUES ('4', 'Nodo Pontevedra', 'Pontevedra' , 'rua valle inclán 5' , '42.433705', '-8.648891', '4' , '4' , '4', 'Funcionando', 'Nada Pendiente');
-INSERT INTO nodos (nombre, ubicacion, direccion_fisica, latitud, longitud, controladora, switch, olt, estado, pendiente)
+INSERT INTO nodos (id, nombre, ubicacion, direccion_fisica, latitud, longitud, controladora, switch, olt, estado, pendiente)
   VALUES ('5', 'Nodo Ourense', 'Ourense' , 'plaza do camiño s/n' , '42.342239', '-7.866024', '5' , '5' , '5', 'Funcionando', 'Nada Pendiente');
-INSERT INTO nodos (nombre, ubicacion, direccion_fisica, latitud, longitud, controladora, switch, olt, estado, pendiente)
+INSERT INTO nodos (id, nombre, ubicacion, direccion_fisica, latitud, longitud, controladora, switch, olt, estado, pendiente)
   VALUES ('6', 'Nodo Lugo', 'Lugo' , 'avda da muralla 5' , '43.011397', '-7.560246', '6' , '6' , '6', 'En construcción', 'Pruebas de red');
 
 
